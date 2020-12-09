74f717955eaf86c9481a160fa7ec8d714a1e380e
yangtingting123456 <3048903923@qq.com>
2020-12-09 17:46:32 +0800

    读取excel合并单元格处理

--- samples/test_excel.py
+++ samples/test_excel.py
@@ -4,7 +4,19 @@ excel_path = os.path.join(os.path.dirname(__file__),'..','data','TestData.xlsx')
 workbook = xlrd.open_workbook(excel_path)
 sheet = workbook.sheet_by_name('Sheet1')
 print(sheet.cell_value(1,0))
-print(sheet.merged_cells)
+print(sheet.merged_cells)  #包含四个元素（起始行，结算行，起始列，介绍列）
+
+#输入行列，判断一个单元格十分始合并过
+x=0
+y=1
+if x>=1 and x<6:
+    if y>=0 and y<1:
+        print('合并单元格')
+    else:
+         print('不是合并单元格')
+else:
+     print('不是合并单元格')
+
 
 
 
--- utils/excel_utls.py
+++ utils/excel_utls.py
@@ -1,14 +1,27 @@
 import xlrd3
 import os
-excel_path = os.path.join(os.path.dirname(__file__),'..','data','TestData.xlsx')
-workbook = xlrd3.open_workbook(excel_path)
+
+workbook = xlrd3.open_workbook(os.path.join(os.path.dirname(__file__),                                            '..','data','TestData.xlsx'))
 sheet = workbook.sheet_by_name('Sheet1')
 # merged_cells 获取当前表格所有合并单元格的位置信息 ，返回一个列表
-merged = sheet.merged_cells
-row_index = 3  ; col_index = 0
-for (rlow, rhigh, clow, chigh) in merged:  # 遍历表格中所有合并单元格位置信息
-    if (row_index >= rlow and row_index < rhigh):  # 行坐标判断
-        if (col_index >= clow and col_index < chigh):  # 列坐标判断
-            # 如果满足条件，就把合并单元格第一个位置的值赋给其它合并单元格
-            cell_value = sheet.cell_value(rlow,clow)
-print( cell_value )
+def get_cell_merge_values(row_index,col_index):
+    cell_value = None
+    merged = sheet.merged_cells
+    for (rlow, rhigh, clow, chigh) in merged:  # 遍历表格中所有合并单元格位置信息
+        if (row_index >= rlow and row_index < rhigh):  # 行坐标判断
+            if (col_index >= clow and col_index < chigh):  # 列坐标判断
+                # 如果满足条件，就把合并单元格第一个位置的值赋给其它合并单元格
+                cell_value = sheet.cell_value(rlow, clow)
+                break;
+            else:
+                cell_value = sheet.cell_value(row_index, col_index)
+        else:
+            cell_value = sheet.cell_value(row_index, col_index)
+    return cell_value
+
+for i in range(0,11):
+    for j in range(0,3):
+        print(get_cell_merge_values(i,j),end=' ')
+    print()
+
+
