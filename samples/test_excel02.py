06aadd49cdeabfc1e8f7d5eca41796bf5adb28a0
yangtingting123456 <3048903923@qq.com>
2020-12-10 16:55:04 +0800

    读取excel合并单元格处理01

---
+++ samples/test_excel02.py
@@ -0,0 +1,45 @@
+import xlrd3
+import os
+workbook = xlrd3.open_workbook(os.path.join(os.path.dirname(__file__),'..','data','TestData.xlsx'))
+sheet = workbook.sheet_by_name('testcase01')
+# merged_cells 获取当前表格所有合并单元格的位置信息 ，返回一个列表
+def get_cell_merge_values(row_index,col_index):
+    cell_value = None
+    merged = sheet.merged_cells
+    for (rlow, rhigh, clow, chigh) in merged:  # 遍历表格中所有合并单元格位置信息
+        if (row_index >= rlow and row_index < rhigh):  # 行坐标判断
+            if (col_index >= clow and col_index < chigh):  # 列坐标判断
+                # 如果满足条件，就把合并单元格第一个位置的值赋给其它合并单元格
+                cell_value = sheet.cell_value(rlow, clow)
+                break;
+            else:
+                cell_value = sheet.cell_value(row_index, col_index)
+        else:
+            cell_value = sheet.cell_value(row_index, col_index)
+    return cell_value
+#
+# for i in range(0,11):
+#     for j in range(0,3):
+#         print(get_cell_merge_values(i,j),end=' ')
+#     print()
+
+# excel_list_data = []
+# row_head = sheet.row_values(0)
+# row_dict = {}
+# row_dict[row_head[0]] = get_cell_merge_values(1,0)
+# row_dict[row_head[1]] = get_cell_merge_values(1,1)
+# row_dict[row_head[2]] = get_cell_merge_values(1,2)
+# row_dict[row_head[3]] = get_cell_merge_values(1,3)
+# print(row_dict)
+
+#步骤二： 改为循环一行
+excel_list_data = []
+row_head = sheet.row_values(0)
+for row_num in range(1,sheet.nrows):
+    row_dict = {}
+    for col_num in range(sheet.ncols):
+        row_dict[row_head[col_num]] = get_cell_merge_values(row_num, col_num)
+    excel_list_data.append(row_dict)
+
+for  data  in excel_list_data:
+    print(data)
--- utils/excel_utls.py
+++ utils/excel_utls.py
@@ -1,27 +1,49 @@
 import xlrd3
 import os
 
-workbook = xlrd3.open_workbook(os.path.join(os.path.dirname(__file__),                                            '..','data','TestData.xlsx'))
-sheet = workbook.sheet_by_name('Sheet1')
-# merged_cells 获取当前表格所有合并单元格的位置信息 ，返回一个列表
-def get_cell_merge_values(row_index,col_index):
-    cell_value = None
-    merged = sheet.merged_cells
-    for (rlow, rhigh, clow, chigh) in merged:  # 遍历表格中所有合并单元格位置信息
-        if (row_index >= rlow and row_index < rhigh):  # 行坐标判断
-            if (col_index >= clow and col_index < chigh):  # 列坐标判断
-                # 如果满足条件，就把合并单元格第一个位置的值赋给其它合并单元格
-                cell_value = sheet.cell_value(rlow, clow)
-                break;
+class ExcelUtils:
+    def __init__(self,excel_file_path,sheet_name):
+        self.excel_file_path = excel_file_path
+        self.sheet_name= sheet_name
+        self.sheet = self.get_sheet()
+
+    def get_sheet(self):
+        '''根据文件路径获取表格对象'''
+        workbook = xlrd3.open_workbook(self.excel_file_path)
+        sheet = workbook.sheet_by_name(self.sheet_name)
+        return sheet
+
+    def get_max_row_number(self):
+        '''获取表格行数'''
+        max_row_number = self.sheet.nrows
+        return max_row_number
+
+    def get_column_count(self):
+        '''获取表格列数'''
+        column_count = self.sheet.ncols
+        return column_count
+
+    def get_merge_cell_values(self,row_index,col_index):
+        '''处理excel单元格的数据，包含合并单元格数据'''
+        cell_value = None
+        merged = self.sheet.merged_cells
+        for (rlow, rhigh, clow, chigh) in merged:  # 遍历表格中所有合并单元格位置信息
+            if (row_index >= rlow and row_index < rhigh):  # 行坐标判断
+                if (col_index >= clow and col_index < chigh):  # 列坐标判断
+                    # 如果满足条件，就把合并单元格第一个位置的值赋给其它合并单元格
+                    cell_value = self.sheet.cell_value(rlow, clow)
+                    break;
+                else:
+                    cell_value = self.sheet.cell_value(row_index, col_index)
             else:
-                cell_value = sheet.cell_value(row_index, col_index)
-        else:
-            cell_value = sheet.cell_value(row_index, col_index)
-    return cell_value
+                cell_value = self.sheet.cell_value(row_index, col_index)
+        return cell_value
 
-for i in range(0,11):
-    for j in range(0,3):
-        print(get_cell_merge_values(i,j),end=' ')
-    print()
+excel_path = os.path.join(os.path.dirname(__file__), '..','data','TestData.xlsx')
+excel_value = ExcelUtils(excel_path,'testcase01')
 
-
+if __name__ == '__main__':
+    for i in range(0, 11):
+        for j in range(0, 3):
+            print(excel_value.get_merge_cell_values(i, j), end=' ')
+        print()
