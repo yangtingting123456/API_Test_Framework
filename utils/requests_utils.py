10182c9cef490a3f8860e03ebadb790da8116bd6
yangtingting123456 <3048903923@qq.com>
2020-12-16 17:55:03 +0800

    根据上一次excel封装的数据进行再一次封装

--- utils/requests_utils.py
+++ utils/requests_utils.py
@@ -38,10 +38,32 @@ class RequestsUtils:
         }
         return result
 
+    def request(self,step_info):
+        request_type = step_info['请求方式']
+        if request_type == 'get':
+            result = self.get(step_info)
+        elif request_type == 'post':
+            result = self.post(step_info)
+        else:
+            result = {'code':1,'result':'请求方式不支持'}
+        return result
+
+    def request_by_step(self,test_steps):
+        for test_step in test_steps:
+            result = self.request(test_step)
+            print(result)
+            if result['code'] != 0:
+                break
+        return result
+
 if __name__ == '__main__':
-    req_get_dict = {'测试用例编号': 'api_case_01', '测试用例名称': '获取access_token接口测试', '用例执行': '是', '用例步骤': 'step01', '接口名称': '获取access_token接口', '请求方式': 'get', '请求头部信息': '', '请求地址': '/cgi-bin/token', '请求参数（get）': '{"grant_type":"client_credential","appid":"wxf26ad2ae7497289a","secret":"177931a321a1f39a47a1ef202d8a3497"}', '请求参数（post）': ''}
-    req_post_dict ={'测试用例编号': 'api_case_03', '测试用例名称': '测试能否正确删除用户标签', '用例执行': '是', '用例步骤': 'step03', '接口名称': '删除标签接口', '请求方式': 'post', '请求头部信息': '', '请求地址': '/cgi-bin/tags/delete', '请求参数（get）': '{"access_token":"40_e-gBgD_JItrxjkaWJa1By0KGI1h6NNS6Nzs5RIpn2mwZf6ykmZobHnmmjN83kIneSJI9KnEOMwEgGyjwvVHtDctQ0EyntOX7wCHT7oWfxF80P1MiUwqnmck6apQBAcvleUvPHHyV7AGB31ACKHKeAGAOLO"}', '请求参数（post)': '{ "tag":{ "id" :456}} '}
-    requestsUtils =RequestsUtils()
-    v = requestsUtils.post(req_post_dict)
-    print(v)
+   step_list = [{'测试用例编号': 'api_case_02', '测试用例名称': '测试能否创建标签接口测试', '用例执行': '是', '用例步骤': 'step01',
+                 '接口名称': '获取access_token接口', '请求方式': 'get', '请求头部信息': '', '请求地址': '/cgi-bin/token',
+                 '请求参数（get）': '{"grant_type":"client_credential","appid":"wxf26ad2ae7497289a","secret":"177931a321a1f39a47a1ef202d8a3497"}', '请求参数（post）': ''},
+                {'测试用例编号': 'api_case_02', '测试用例名称': '测试能否创建标签接口测试', '用例执行': '是', '用例步骤': 'step02', '接口名称': '创建标签接口', '请求方式': 'post', '请求头部信息': '',
+                 '请求地址': 'cgi-bin/tags/create', '请求参数（get）':
+                 '{"access_token":""}', '请求参数（post）': '{"tag":{"name" :"11"}}'}]
+   requestsUtils =RequestsUtils()
+   v = requestsUtils.request_by_step(step_list)
+   print(v)
 
