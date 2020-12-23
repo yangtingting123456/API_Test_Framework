998b4d84393d0590760bdc63ac85859d939ea57e
yangtingting123456 <3048903923@qq.com>
2020-12-23 17:57:23 +0800

    excel接口测试用例关联起来1111

--- samples/deom/demo02.py
+++ samples/deom/demo02.py
@@ -2,17 +2,17 @@ import re
 import ast
 str1 = '{"access_tokne":${token}}'
 variables_list = re.findall('\\${\w+}',str1)
-
-dict1 = {'token':'adasd'}
+print(variables_list)
+dict1 = {"tag":{"token":121,"name":"342534asdf"}}
 print(dict1[variables_list[0][2:-1]])
 str1 = str1.replace(variables_list[0],'"%s"'%dict1[variables_list[0][2:-1]])
 print(str1)
 #成果：{"access_tokne":${token}} 考虑：多个变量怎么办？不需要替换的情况怎么办
-str2 = '{"name":${n},"age":${a}}'
-dict2 = {'n':'xiaoming','a':18}
-variables_list2 = re.findall('\\${\w+}',str2)
-print(variables_list2)
-for v in variables_list2:
-    str2 = str2.replace(v,'"%s"'%dict2[v[2:-1]])
-print(str2)
+# str2 = '{"name":${n},"age":${a}}'
+# dict2 = {'n':'xiaoming','a':18}
+# variables_list2 = re.findall('\\${\w+}',str2)
+# print(variables_list2)
+# for v in variables_list2:
+#     str2 = str2.replace(v,'"%s"'%dict2[v[2:-1]])
+# print(str2)
 
--- utils/requests_utils.py
+++ utils/requests_utils.py
@@ -23,6 +23,9 @@ class RequestsUtils:
         if requests_info['取值方式'] == 'jsonpath取值':
             value = jsonpath.jsonpath(response.json(),requests_info['取值代码'])[0]
             self.tmp_variables[requests_info['取值变量']] = value
+        elif requests_info['取值方式'] == '正则取值':
+            value = re.findall(requests_info['取值代码'], response.text)[0]
+            self.tmp_variables[requests_info['取值变量']] = value
         result = {
             'code':0,
             'response_code':response.status_code,
@@ -34,14 +37,21 @@ class RequestsUtils:
 
     def __post(self,requests_info):
         url = self.hosts+requests_info['请求地址']
-        variable_list = re.findall('\\${\w+}', requests_info['请求参数(get)'])
-        for v in variable_list:
-            requests_info['请求参数(get)'] = requests_info['请求参数(get)'].replace(v,
-                                                                            '"%s"' % self.tmp_variables[v[2:-1]])
-        response = self.session.post(url = url,
-                                     params = json.loads(requests_info['请求参数(get)']),
-                                     json = json.loads(requests_info['请求参数(post)']),
-                                     headers = requests_info['请求头部信息'])
+        get_variable_list = re.findall('\\${\w+}', requests_info['请求参数(get)'])
+        for variable in get_variable_list:
+            requests_info['请求参数(get)'] = requests_info['请求参数(get)'].replace(variable,
+                                                                            '"%s"' % self.tmp_variables[variable[2:-1]])
+        post_variable_list = re.findall('\\${\w+}', requests_info['请求参数(post)'])
+        for variable in post_variable_list:
+            requests_info['请求参数(post)'] = requests_info['请求参数(post)'].replace(variable,
+                                                                              '"%s"'%self.tmp_variables[variable[2:-1]])
+        response = self.session.post(url=url,
+                                     headers=requests_info['请求头部信息'],
+                                     params=json.loads(requests_info['请求参数(get)']),
+                                     data=json.dumps(json.loads(requests_info['请求参数(post)']),
+                                                     ensure_ascii=False).encode('utf-8')
+                                     # json = json.loads(requests_info['请求参数(post)'])
+                                     )
         response.encoding = response.apparent_encoding  #保证不乱吗
         if requests_info['取值方式'] == 'jsonpath取值':
             value = jsonpath.jsonpath(response.json(),requests_info['取值代码'])[0]
@@ -74,16 +84,11 @@ class RequestsUtils:
             result = self.request(test_step)
             if result['code'] != 0:
                 break
+            print(result['response_body'])
         return result
 
 if __name__ == '__main__':
-   # step_list = [{'测试用例编号': 'api_case_02', '测试用例名称': '测试能否创建标签接口测试', '用例执行': '是', '用例步骤': 'step01',
-   #               '接口名称': '获取access_token接口', '请求方式': 'get', '请求头部信息': '', '请求地址': '/cgi-bin/token',
-   #               '请求参数（get）': '{"grant_type":"client_credential","appid":"wxf26ad2ae7497289a","secret":"177931a321a1f39a47a1ef202d8a3497"}', '请求参数（post）': ''}, {'测试用例编号': 'api_case_02', '测试用例名称': '测试能否创建标签接口测试', '用例执行': '是', '用例步骤': 'step02', '接口名称': '创建标签接口', '请求方式': 'post', '请求头部信息': '', '请求地址': 'cgi-bin/tags/create', '请求参数（get）':
-   #           '{"access_token":"$access_token"}', '请求参数（post）': '{   "tag" : {     "name" : "广东"//标签名   } } '}]
-   # requestsUtils =RequestsUtils()
-   # v = requestsUtils.request_by_step(step_list)
-   # print(v)
+
 
    #  step_list1 ={'测试用例编号': 'api_case_02', '测试用例名称': '创建标签接口测试', '用例执行': '否', '用例步骤': 'step_01', '接口名称': '获取access_token接口', '请求方式': 'get', '请求头部信息': '', '请求地址': '/cgi-bin/token', '请求参数(get)': '{"grant_type":"client_credential","appid":"wx55614004f367f8ca","secret":"65515b46dd758dfdb09420bb7db2c67f"}', '请求参数(post)': '', '取值方式': 'jsonpath取值', '取值代码': '"access_token":"(.+?)"', '取值变量': 'token', '断言类型': 'json_key_value', '期望结果': '{"expires_in":7200}'}
    #  step_list2 = {'测试用例编号': 'api_case_02', '测试用例名称': '创建标签接口测试', '用例执行': '否', '用例步骤': 'step_02', '接口名称': '创建标签接口', '请求方式': 'post', '请求头部信息': '', '请求地址': '/cgi-bin/tags/create', '请求参数(get)': '{"access_token":"40_ni-zkk10K0nRlEp7b4J6oPZDvYoSDS9TODX58cO3e6T5ZdE4SZ-PmIBVp6tuDN7-r3RYAcbVqvpUHpTXyBdQtaf7QcO0isGQnFsUL_siHRUFBzNecAL5iUK_alrfoWB66pFLEEmO4SFo6_t_ZZXiACADDB"}', '请求参数(post)': '{   "tag" : {     "name" : "2"} }', '取值方式': '无', '取值代码': '', '取值变量': '', '断言类型': 'json_key', '期望结果': 'tag'}
@@ -108,11 +113,31 @@ if __name__ == '__main__':
 
    #接口关联测试
 
-   step_list =[
-       {'测试用例编号': 'api_case_02', '测试用例名称': '创建标签接口测试', '用例执行': '否', '用例步骤': 'step_01', '接口名称': '获取access_token接口', '请求方式': 'get', '请求头部信息': '', '请求地址': '/cgi-bin/token', '请求参数(get)': '{"grant_type":"client_credential","appid":"wxf26ad2ae7497289a","secret":"177931a321a1f39a47a1ef202d8a3497"}', '请求参数(post)': '', '取值方式': 'jsonpath取值', '取值代码': '$.access_token', '取值变量': 'token', '断言类型': 'json_key_value', '期望结果': '{"expires_in":7200}'},
-       {'测试用例编号': 'api_case_02', '测试用例名称': '创建标签接口测试', '用例执行': '否', '用例步骤': 'step_02', '接口名称': '创建标签接口', '请求方式': 'post', '请求头部信息': '', '请求地址': '/cgi-bin/tags/create', '请求参数(get)': '{"access_token":${token}}', '请求参数(post)': '{   "tag" : {     "name" : "广东aaaa" } } ', '取值方式': '无', '取值代码': '', '取值变量': '', '断言类型': 'json_key', '期望结果': 'tag'}
-   ]
+   # step_list =[
+   #     {'测试用例编号': 'api_case_02', '测试用例名称': '创建标签接口测试', '用例执行': '否', '用例步骤': 'step_01', '接口名称': '获取access_token接口', '请求方式': 'get', '请求头部信息': '', '请求地址': '/cgi-bin/token', '请求参数(get)': '{"grant_type":"client_credential","appid":"wxf26ad2ae7497289a","secret":"177931a321a1f39a47a1ef202d8a3497"}', '请求参数(post)': '', '取值方式': 'jsonpath取值', '取值代码': '$.access_token', '取值变量': 'token', '断言类型': 'json_key_value', '期望结果': '{"expires_in":7200}'},
+   #     {'测试用例编号': 'api_case_02', '测试用例名称': '创建标签接口测试', '用例执行': '否', '用例步骤': 'step_02', '接口名称': '创建标签接口', '请求方式': 'post', '请求头部信息': '', '请求地址': '/cgi-bin/tags/create', '请求参数(get)': '{"access_token":${token}}', '请求参数(post)': '{   "tag" : {     "name" : "aa1231231" } } ', '取值方式': '无', '取值代码': '', '取值变量': '', '断言类型': 'json_key', '期望结果': 'tag'}
+   # ]
 
+   # step_list = [
+   #     {'测试用例编号': 'api_case_03', '测试用例名称': '删除标签接口测试', '用例执行': '是', '用例步骤': 'step_01', '接口名称': '获取access_token接口', '请求方式': 'get', '请求头部信息': '', '请求地址': '/cgi-bin/token', '请求参数(get)': '{"grant_type":"client_credential","appid":"wxf26ad2ae7497289a","secret":"177931a321a1f39a47a1ef202d8a3497"}', '请求参数(post)': '', '取值方式': 'jsonpath取值', '取值代码': '$.access_token', '取值变量': 'token', '断言类型': 'json_key', '期望结果': 'access_token'},
+   #     {'测试用例编号': 'api_case_03', '测试用例名称': '删除标签接口测试', '用例执行': '是', '用例步骤': 'step_02', '接口名称': '创建标签接口', '请求方式': 'post', '请求头部信息': '', '请求地址': '/cgi-bin/tags/create', '请求参数(get)': '{"access_token":${token}}', '请求参数(post)': '{"tag" :{"name" :"3456"}} ', '取值方式': '"id":(.+?),', '取值代码': 'tag_id', '取值变量': 'tag_id', '断言类型': 'json_key', '期望结果': ''},
+   #     {'测试用例编号': 'api_case_03', '测试用例名称': '删除标签接口测试', '用例执行': '是', '用例步骤': 'step_03', '接口名称': '删除标签接口', '请求方式': 'post', '请求头部信息': '', '请求地址': '/cgi-bin/tags/delete', '请求参数(get)': '{"access_token":${token}}', '请求参数(post)': '{"tag":{"id" : ${tag_id}}}', '取值方式': '无', '取值代码': '', '取值变量': '', '断言类型': 'json_key_value', '期望结果': '{"errcode":0}'}
+   # ]
+   step_list = [
+       {'测试用例编号': 'api_case_3', '测试用例名称': '删除标签接口测试', '用例执行': '是', '用例步骤': 'step_01', '接口名称': '获取access_token接口',
+        '请求方式': 'get', '请求头部信息': '', '请求地址': '/cgi-bin/token',
+        '请求参数(get)': '{"grant_type":"client_credential","appid":"wxf26ad2ae7497289a","secret":"177931a321a1f39a47a1ef202d8a3497"}',
+        '请求参数(post)': '', '取值方式': 'jsonpath取值', '取值代码': '$.access_token', '取值变量': 'token', '断言类型': 'json_key',
+        '期望结果': 'access_token'},
+       {'测试用例编号': 'api_case_3', '测试用例名称': '删除标签接口测试', '用例执行': '是', '用例步骤': 'step_02', '接口名称': '创建标签接口', '请求方式': 'post',
+        '请求头部信息': '', '请求地址': '/cgi-bin/tags/create', '请求参数(get)': '{"access_token":${token}}',
+        '请求参数(post)': '{"tag":{"name":"adf"}}', '取值方式': '正则取值', '取值代码': '"id":(.+?),', '取值变量': 'tag_id',
+        '断言类型': 'json_key', '期望结果': ''},
+       {'测试用例编号': 'api_case_3', '测试用例名称': '删除标签接口测试', '用例执行': '是', '用例步骤': 'step_03', '接口名称': '删除标签接口', '请求方式': 'post',
+        '请求头部信息': '', '请求地址': '/cgi-bin/tags/delete', '请求参数(get)': '{"access_token":${token}}',
+        '请求参数(post)': '{"tag":{"id":126}}', '取值方式': '无', '取值代码': '', '取值变量': '', '断言类型': 'json_key_value',
+        '期望结果': '{"errcode":0}'}
+   ]
    requestsUtils = RequestsUtils()
    v=requestsUtils.request_by_step( step_list )
    print( v )
